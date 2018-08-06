aa5bf8a7803b95ef61fa9343c6cb750a208a8503
t_1493606483941_0930 <>
2018-08-06 15:36:32 +0800

    去除测试lang()

--- application/common/model/Message.php
+++ application/common/model/Message.php
@@ -8,7 +8,6 @@ class Message extends Base {
     protected $observerClass = 'app\admin\event\cms\Message';
 
     public function getHeaderList() {
-        lang();
         $rows['num'] = $this->count();
         $rows['list'] = $this->order('id desc')->limit(2)->select();
         return $rows;
